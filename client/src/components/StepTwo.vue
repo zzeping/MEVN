a4d5154c19e3d774f18ed6ead65475c882180d1b
zzeping <zzeping34@gmail.com>
2023-02-23 21:44:50 +0100

    step two plot three graphs

--- client/src/components/StepTwo.vue
+++ client/src/components/StepTwo.vue
@@ -1,8 +1,10 @@
 
 <template>
-    <div>
-        <v-card class="mb-12" height="400px">
-            <svg ref="chart"></svg>
+    <v-container class="pl-10 pr-10">
+        <v-card class="mb-12 pl-8" height="490px">
+            <svg ref="chart1"></svg>
+            <svg ref="chart2"></svg>
+            <svg ref="chart3"></svg>
         </v-card>
 
         <v-btn color="primary" @click="nextStep">
@@ -10,7 +12,7 @@
         </v-btn>
 
         <v-btn text @click="previousStep">Back</v-btn>
-    </div>
+    </v-container>
 </template>
 
 <script>
@@ -22,6 +24,8 @@ export default {
         return {
             torques: [],
             times: [],
+            posAnats: [],
+            velocities: [],
             fileContent: '',
         }
     },
@@ -36,7 +40,9 @@ export default {
     watch: {
         'formData.dataFile'(new_file) {
             if (new_file != null) {
-                d3.select(this.$refs.chart).selectAll('g').remove();
+                d3.select(this.$refs.chart1).selectAll('g').remove();
+                d3.select(this.$refs.chart2).selectAll('g').remove();
+                d3.select(this.$refs.chart3).selectAll('g').remove();
                 this.loadDataFile(new_file);
             }
         }
@@ -65,10 +71,9 @@ export default {
                     });
                 this.times = data.map(point => parseFloat(point.time));
                 this.torques = data.map(point => parseFloat(point.torque));
-                const posAnats = data.map(point => parseFloat(point.posAnat));
-                const velocities = data.map(point => parseFloat(point.velocity));
-                console.log(this.times);
-                console.log(this.torques);
+                this.posAnats = data.map(point => parseFloat(point.posAnat));
+                this.velocities = data.map(point => parseFloat(point.velocity));
+
                 this.drawChart();
             };
             reader.readAsText(file);
@@ -76,9 +81,21 @@ export default {
         drawChart() {
             const margin = { top: 20, right: 20, bottom: 30, left: 50 };
             const width = 900 - margin.left - margin.right;
-            const height = 300 - margin.top - margin.bottom;
+            const height = 150 - margin.top - margin.bottom;
 
-            const svg = d3.select(this.$refs.chart)
+            const svg1 = d3.select(this.$refs.chart1)
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+            const svg2 = d3.select(this.$refs.chart2)
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+            const svg3 = d3.select(this.$refs.chart3)
                 .attr('width', width + margin.left + margin.right)
                 .attr('height', height + margin.top + margin.bottom)
                 .append('g')
@@ -88,29 +105,66 @@ export default {
                 .domain(d3.extent(this.times))
                 .range([0, width]);
 
-            const y = d3.scaleLinear()
+            const y1 = d3.scaleLinear()
                 .domain(d3.extent(this.torques))
+                .range([height, 0]);
+            const y2 = d3.scaleLinear()
+                .domain(d3.extent(this.posAnats))
+                .range([height, 0]);
+            const y3 = d3.scaleLinear()
+                .domain(d3.extent(this.velocities))
                 .range([height, 0]);
 
             const xAxis = d3.axisBottom(x);
 
-            const yAxis = d3.axisLeft(y);
+            const yAxis1 = d3.axisLeft(y1);
+            const yAxis2 = d3.axisLeft(y2);
+            const yAxis3 = d3.axisLeft(y3);
 
-            svg.append('g')
+            svg1.append('g')
                 .attr('transform', `translate(0, ${height})`)
                 .call(xAxis);
-            svg.append('g')
-                .call(yAxis)
-
-            svg.append('path')
+            svg1.append('g')
+                .call(yAxis1)
+            svg1.append('path')
                 .datum(this.torques)
                 .attr('fill', 'none')
                 .attr('stroke', 'steelblue')
                 .attr('stroke-width', 1.5)
                 .attr('d', d3.line()
                     .x((d, i) => x(this.times[i]))
-                    .y(d => y(d))
+                    .y(d => y1(d))
                 );
+            svg2.append('g')
+                .attr('transform', `translate(0, ${height})`)
+                .call(xAxis);
+            svg2.append('g')
+                .call(yAxis2)
+            svg2.append('path')
+                .datum(this.posAnats)
+                .attr('fill', 'none')
+                .attr('stroke', 'steelblue')
+                .attr('stroke-width', 1.5)
+                .attr('d', d3.line()
+                    .x((d, i) => x(this.times[i]))
+                    .y(d => y2(d))
+                );
+
+            svg3.append('g')
+                .attr('transform', `translate(0, ${height})`)
+                .call(xAxis);
+            svg3.append('g')
+                .call(yAxis3)
+            svg3.append('path')
+                .datum(this.velocities)
+                .attr('fill', 'none')
+                .attr('stroke', 'steelblue')
+                .attr('stroke-width', 1.5)
+                .attr('d', d3.line()
+                    .x((d, i) => x(this.times[i]))
+                    .y(d => y3(d))
+                );
+
 
         },
 
