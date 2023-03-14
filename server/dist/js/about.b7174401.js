a131048e79cbfe23bbd5d0ad3e8ed97eb317f36e
zzeping <zzeping34@gmail.com>
2023-03-14 13:32:58 +0100

    server change

--- server/dist/js/about.b7174401.js
+++ server/dist/js/about.b7174401.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[443],{2028:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=function(){var n=this,u=n._self._c;return u("h1",[n._v("About page")])},l=[],c=e(1001),r={},s=(0,c.Z)(r,t,l,!1,null,null,null),f=s.exports}}]);
-//# sourceMappingURL=about.d831e1e0.js.map
+//# sourceMappingURL=about.b7174401.js.map
