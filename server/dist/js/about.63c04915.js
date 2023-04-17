675acc99bc02ac6dafb329a2c9fdba48697387e6
zzeping <zzeping34@gmail.com>
2023-04-18 02:33:24 +0800

    report,default image

--- server/dist/js/about.63c04915.js
+++ server/dist/js/about.63c04915.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[443],{2028:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=function(){var n=this,u=n._self._c;return u("h1",[n._v("About page")])},l=[],c=e(1001),r={},s=(0,c.Z)(r,t,l,!1,null,null,null),f=s.exports}}]);
-//# sourceMappingURL=about.d16de8f2.js.map
+//# sourceMappingURL=about.63c04915.js.map
